035e46573879f2a8290bfb511695e8ee1bb22bc4
Anastasya Sinenko <nastasinenko5@gmail.com>
2025-09-22 11:36:42 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,84 +1,71 @@
-function countLetter(str, letter) {
-  let count = 0;
-  for (let i = 0; i < str.length; i++) {
-    if (str.charAt(i).toLowerCase() === letter.toLowerCase()) {
-      count++;
+class Pokemon {
+    constructor(name, selector) {
+        this.name = name;
+        this.maxHp = 100;
+        this.hp = this.maxHp;
+        this.selector = selector;
     }
-  }
-  return count;
-}
 
-function getRow(firstRow, secondRow, letter = "a") {
-  const firstCount = countLetter(firstRow, letter);
-  const secondCount = countLetter(secondRow, letter);
+    
+    changeHp(damage) {
+        this.hp = Math.max(this.hp - damage, 0);
+        this.renderHP();
+        if (this.hp === 0) {
+            alert(this.name + " програв!");
+        }
+    }
 
-  return firstCount > secondCount ? firstRow : secondRow;
-}
+    renderHP() {
+        const healthText = document.getElementById(`health-${this.selector}`);
+        const progressbar = document.getElementById(`progressbar-${this.selector}`);
+        healthText.textContent = `${this.hp} / ${this.maxHp}`;
+        progressbar.style.width = (this.hp / this.maxHp * 100) + "%";
 
-
-function task1() {
-  const firstRow = "Slow and steady wins the race";
-  const secondRow = "You can say that again";
-
-  let letter = prompt("Введіть букву для підрахунку:", "");
-  if (!letter) return;
-
-  let result = getRow(firstRow, secondRow, letter);
-  alert(`Рядок з більшою кількістю "${letter}": \n${result}`);
-}
-
-function formattedPhone(phone) {
-  if (!phone && phone !== '') return "Неправильний формат номера!";
-
-  // Беремо тільки цифри
-  const digits = phone.replace(/\D/g, '');
-
-  let local = null;
-
- 
-
-  
-  if (/^\+380\d{9}$/.test(phone)) {
-    local = '0' + digits.slice(3); 
-  }
- 
-  else if (/^380\d{9}$/.test(digits)) {
-    local = '0' + digits.slice(3);
-  }
-  
-  else if (/^80\d{9}$/.test(digits)) {
-    local = digits.slice(1); 
-  }
- 
-  else if (/^0\d{9}$/.test(digits)) {
-    local = digits; // "0671234567"
-  }
-  else {
-    return "Неправильний формат номера!";
-  }
-
-  
-  const code = local.slice(0, 3);
-  const part1 = local.slice(3, 6);
-  const part2 = local.slice(6, 8);
-  const part3 = local.slice(8, 10);
-
-  return `+38 (${code}) ${part1}-${part2}-${part3}`;
+       
+        progressbar.classList.remove("low", "critical");
+        if (this.hp / this.maxHp <= 0.2) {
+            progressbar.classList.add("critical");
+        } else if (this.hp / this.maxHp <= 0.5) {
+            progressbar.classList.add("low");
+        }
+    }
 }
 
 
-function task2() {
-  let phone = prompt("Введіть номер телефону:");
-  if (!phone) return;
+const player1 = new Pokemon("Pikachu", "character");
+const player2 = new Pokemon("Charmander", "enemy");
 
-  let result = formattedPhone(phone);
-  alert(`Результат:\n${result}`);
+
+player1.renderHP();
+player2.renderHP();
+
+
+function attack(attacker, defender, maxDamage, minDamage = 5) {
+    const damage = Math.floor(Math.random() * (maxDamage - minDamage + 1)) + minDamage;
+    console.log(`${attacker.name} атакує ${defender.name} і завдає ${damage} шкоди!`);
+    defender.changeHp(damage);
 }
 
 
-console.log(formattedPhone('+380664567890')); // +38 (066) 456-78-90
-console.log(formattedPhone('80664567890'));   // +38 (066) 456-78-90
-console.log(formattedPhone('80971234567'));   // +38 (097) 123-45-67
-console.log(formattedPhone('0671234567'));    // +38 (067) 123-45-67
+const $btnKick = document.getElementById("btn-kick");
 
-console.log(formattedPhone('123456'));    
+
+$btnKick.addEventListener("click", function () {
+    attack(player1, player2, 20, 5);
+    if (player2.hp > 0) {
+        attack(player2, player1, 20, 5);
+    }
+});
+
+
+const $btnSpecial = document.createElement("button");
+$btnSpecial.classList.add("button");
+$btnSpecial.textContent = "Special Attack";
+document.querySelector(".control").appendChild($btnSpecial);
+
+$btnSpecial.addEventListener("click", function () {
+    attack(player1, player2, 40, 15);
+    if (player2.hp > 0) {
+        attack(player2, player1, 20, 5);
+    }
+});
